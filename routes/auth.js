e5199b03fd77dc01753aae223e5c17fd6e11a555
Ivan <151129749+ivwelln@users.noreply.github.com>
2025-06-19 13:09:15 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const multer = require('multer');
 const path = require('path');
 const User = require('../models/User');
